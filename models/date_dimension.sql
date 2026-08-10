eb23dcafcad356651c594f263d7bf2e1ec4e1c87
agata.galczynska@gmail.com <agata.galczynska@gmail.com>
2026-08-10 14:39:39 +0000

    daily weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,7 +1,11 @@
 with cte as (
     select
-      STARTED_AT,
-      TO_TIMESTAMP(started_at)
+      TO_TIMESTAMP(started_at) as STARTED_AT,
+      date(TO_TIMESTAMP(started_at)) as date_started_at,
+      hour(TO_TIMESTAMP(started_at)) as hour_started_at,
+      {{day_type('STARTED_AT')}} as day_type,
+      {{get_season('STARTED_AT')}} as station_of_year
+
     from {{ source('demo', 'bike') }}
     where started_at != 'started_at'
 )
